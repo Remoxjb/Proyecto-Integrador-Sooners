09437012112116c33d139453cd43f2cd0542e72c
Juan <juan.badillo.v@gmail.com>
2022-05-31 00:45:01 -0500

    validacion formulario subir archivo

--- assets/js/importar.js
+++ assets/js/importar.js
@@ -93,3 +93,109 @@ function processFile(file) {
             document.querySelector(`#${id} .status-text`).innerHTML = `<span class = "failure">El archivo no se subió correctamente...</span>`;
         } 
 }  */
+
+const expresiones = {
+    nombreD: /^[a-zA-ZÀ-ÿ\s]{1,20}$/,
+    orientacion: /^[a-zA-ZÀ-ÿ\s-]{1,15}$/,
+    secciones: /[0-9]{2}$/,
+    formularios: /^[a-zA-ZÀ-ÿ\s,.]{1,50}$/,
+    cuenta: /[0-9,.]{1,8}$/,
+}
+
+const formulario = document.querySelector('form');
+//console.log(formulario);
+const inputs = document.querySelectorAll('input');
+//console.log(inputs);
+
+const nombreDisenioError= document.querySelector('#errorNombreDisenio');
+const orientacionError = document.querySelector('#errorOrientacion');
+const seccionesError = document.querySelector('#errorSecciones');
+const formularioError = document.querySelector('#errorFormularios');
+const cuentaError = document.querySelector('#errorCuenta');
+
+
+const estatusInf = {
+	nombreD: false,
+	orientacion: false,
+    secciones: false,
+    formularios: false,
+    cuenta: false,
+}
+
+inputs.forEach((inp)=>{
+   // console.log(inp);
+
+    
+    inp.addEventListener("keyup", (e)=>{
+       // console.log(e.target.name);
+        switch(e.target.name){
+            case "nombreDisenio":
+                //console.log(e.target.value);
+                //utilizar expresiones regulares en javascript
+                if(expresiones.nombreD.test(e.target.value)){
+                    estatusInf.nombreD = true
+                    nombreDisenioError.textContent = ""
+                }
+                else{
+                    estatusInf.nombreD = false
+                    nombreDisenioError.textContent = "Nombre no valido"
+                }
+                break
+
+            case "orientacion":
+                if(expresiones.orientacion.test(e.target.value)){
+                    estatusInf.orientacion = true
+                    orientacionError.textContent = ""
+                }
+                else{
+                    estatusInf.orientacion = false
+                    orientacionError.textContent = "Tipo orientacion no valido"
+                }
+                break
+            case "secciones":
+                if(expresiones.secciones.test(e.target.value)){
+                    estatusInf.secciones = true
+                    seccionesError.textContent = ""
+                }
+                else{
+                    estatusInf.secciones = false
+                    seccionesError.textContent = "Número de secciones no validas"
+                }
+                break
+            case "formularios":
+                if(expresiones.formularios.test(e.target.value)){
+                    estatusInf.formularios = true
+                    formularioError.textContent = ""
+                }
+                else{
+                    estatusInf.formularios = false
+                    formularioError.textContent = "Tipo de formulario no valido"
+                }
+                break
+            case "cuenta":
+                if(expresiones.cuenta.test(e.target.value)){
+                    estatusInf.cuenta = true
+                    cuentaError.textContent = ""
+                }
+                else{
+                    estatusInf.cuenta = false
+                    cuentaError.textContent = "Cuenta no valida"
+                }
+                break
+        }
+    })
+}) 
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    //console.log(formulario);
+
+   if(!Object.values(estatusInf).includes(false)){
+       //console.log("Enviado");
+        const datos = Object.fromEntries(
+            new FormData(e.target)
+        )
+        console.log(datos);
+        formulario.reset()
+    }
+})
