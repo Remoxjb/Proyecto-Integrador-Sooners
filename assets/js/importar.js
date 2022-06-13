8e928c4627c263a3620036d389aff5256bdf4765
AleRangel <ale.andrade.rangel@gmail.com>
2022-06-13 08:45:05 -0500

    subirAchivo

--- assets/js/importar.js
+++ assets/js/importar.js
@@ -6,60 +6,59 @@ const
     input = dropArea.querySelector("input");
 
 
-let file; //this is a global variable and we'll use it inside multiple functions
-
+let file;
 button.onclick = () => {
-    input.click(); //if user click on the button then the input also clicked
+    input.click();
 }
 
 input.addEventListener("change", (e) => {
-    //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+
     file = input.files[0];
     dropArea.classList.add("active");
 
     uploadFile(file);
 
 
-    showFile(); //calling function
+    showFile();
 
 });
 
 
-//If user Drag File Over DropArea
+
 dropArea.addEventListener("dragover", (e) => {
-    e.preventDefault(); //preventing from default behaviour
+    e.preventDefault();
     dropArea.classList.add("active");
     dragText.textContent = "Suelta para cargar archivo";
 });
 
-//If user leave dragged File from DropArea
+
 dropArea.addEventListener("dragleave", () => {
     dropArea.classList.remove("active");
     dragText.textContent = "Arrastrar y suelta para cargar archivo";
 });
 
-//If user drop File on DropArea
+
 dropArea.addEventListener("drop", (e) => {
-    e.preventDefault(); //preventing from default behaviour
-    //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+    e.preventDefault();
+
     file = e.dataTransfer.files[0];
 
 
-    showFile(); //calling function
+    showFile();
 
 
 });
 
 function showFile() {
-    let fileType = file.type; //getting selected file type
-    let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
-    if (validExtensions.includes(fileType)) { //if user selected file is an image file
-        let fileReader = new FileReader(); //creating new FileReader object
+    let fileType = file.type;
+    let validExtensions = ["image/jpeg", "image/jpg", "image/png"];
+    if (validExtensions.includes(fileType)) {
+        let fileReader = new FileReader();
         fileReader.onload = () => {
-            let fileURL = fileReader.result; //passing user file source in fileURL variable
-            // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
-            let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
-            dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+            let fileURL = fileReader.result;
+            D IT
+            let imgTag = `<img src="${fileURL}" alt="image">`;
+            dropArea.innerHTML = imgTag;
         }
         fileReader.readAsDataURL(file);
         uploadFile(file);
